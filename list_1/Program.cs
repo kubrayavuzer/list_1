030139f3a71750b8dea7876ef02dd0c17af56972
kubra <kubra@DESKTOP-K1F6O3C>
2024-08-19 02:52:22 +0300

    Proje dosyası ekle.

---
+++ list_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic; //list sınıfını kullanabilmek için bu namespace'si ekliyoruz
+
+class Program
+{
+    public static void Main()
+    {
+        List <string> davetliler = new List <string> ();
+
+        //listeye davetli ekle
+        davetliler.Add("Bülent Ersoy");
+        davetliler.Add("Ajda Pekkan");
+        davetliler.Add("Ebru Gündeş");
+        davetliler.Add("Hadise");
+        davetliler.Add("Hande Yener");
+        davetliler.Add("Tarkan");
+        davetliler.Add("Funda Arar");
+        davetliler.Add("Demet Akalın");
+
+        foreach (string davetli in davetliler)
+        {
+            Console.WriteLine (davetli);
+        }
+    }
+}
+
